7e978f737e7903bb6d8105925d75be75106dc064
Eric Flumerfelt <eflumerf@fnal.gov>
2024-12-12 15:29:16 -0600

    Add artdaq bundle, v3_15_00

--- packages/artdaq/package.py
+++ packages/artdaq/package.py
@@ -30,6 +30,7 @@ class Artdaq(CMakePackage):
 
 
     version("develop", branch="develop", get_full_repo=True)
+    version("v3_15_00", commit="3f67f5216e183e0701e14db4b2e7a45877579a9d")   
     version("v3_14_01", commit="1d6e7037dd85c31525f863e035f0b423367d637b")   
     version("v3_14_00", commit="2006c00ed3c2480ecbea6e18fb8f5711df7529c2")    
     version("v3_13_01", commit="4eb46d5bd3b4f00973f99114efe66428c6f44626")    
