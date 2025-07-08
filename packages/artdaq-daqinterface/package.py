064f5bc032c718f30d7ffbdd663aab4ece763dfa
Your Name <you@example.com>
2025-07-08 10:29:19 -0500

    Improve bookkeeping

--- packages/artdaq-daqinterface/package.py
+++ packages/artdaq-daqinterface/package.py
@@ -60,4 +60,4 @@ class ArtdaqDaqinterface(CMakePackage):
         env.set("ARTDAQ_DAQINTERFACE_VERSION", "SPACK") # Needed by source_me
 
     patch("patch/v3_13_00.path", when="@v3_13_00")
-    patch("patch/v3_14_00.path", when="@v3_14_00")
+    patch("patch/v3_14_00a.path", when="@v3_14_00")
