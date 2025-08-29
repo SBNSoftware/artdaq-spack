27edb3097c5f27e83a345c8a143c44b3f1f77b64
Eric Flumerfelt <eflumerf@fnal.gov>
2025-08-29 14:27:30 -0500

    Update artdaq-utilities for new Doxygen parsing

--- packages/artdaq-suite/package.py
+++ packages/artdaq-suite/package.py
@@ -14,6 +14,7 @@ class ArtdaqSuite(BundlePackage):
     homepage="https://github.com/art-daq"
 
     version("develop")
+    version("v4_03_01")
     version("v4_03_00")
     version("v4_02_00")
     version("v4_01_00")
@@ -66,6 +67,24 @@ class ArtdaqSuite(BundlePackage):
         depends_on("qt +gui+shared")
         depends_on("librdkafka")
         depends_on("epics-base", when="+epics")
+    with when("@v4_03_01"):
+        depends_on("trace@v3_19_00")
+        depends_on("artdaq-core@v4_03_00")
+        depends_on("artdaq-utilities@v2_01_01")
+        depends_on("artdaq-mfextensions@v2_01_00")
+        depends_on("artdaq@v4_03_00")
+        depends_on("artdaq-epics-plugin@v2_00_00", when="+epics")
+        depends_on("artdaq-daqinterface@v4_02_00")
+        depends_on("artdaq-core-demo@v2_02_00", when="+demo")
+        depends_on("artdaq-database@v2_00_00", when="+db")
+        depends_on("artdaq-demo@v4_03_00", when="+demo")
+        # External Dependencies not in art-suite
+        depends_on("swig@4.1.1")
+        depends_on("xmlrpc-c@1.51.06 +curl")
+        depends_on("curl@7.76.1")
+        depends_on("qt@5.15.15 +gui+shared")
+        depends_on("librdkafka@2.8.0")
+        depends_on("epics-base@7.0.6.1", when="+epics")
     with when("@v4_03_00"):
         depends_on("trace@v3_19_00")
         depends_on("artdaq-core@v4_03_00")
